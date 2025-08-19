9e0cf82a26bd2d0c6b952f81d96f43a1438dfe46
hochmuch <egorrussian56@gmail.com>
2025-08-19 16:57:04 +0300

    Task 4 added

---
+++ 4.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+)
+
+func recordConstantly4(ctx context.Context, channel chan int) {
+	i := 0
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if ctx.Err() != nil {
+				return
+			}
+			channel <- i
+			i++
+		}
+	}
+}
+
+func worker4(ctx context.Context, id int, channel chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if ctx.Err() != nil {
+				return
+			}
+			x := <-channel
+			fmt.Printf("Worker #%v прочитал: %v\n", id, x)
+		}
+	}
+}
+
+func main() {
+	n, _ := strconv.Atoi(os.Args[1])
+	channel := make(chan int)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		<-c
+		fmt.Println("Прерывание Ctrl-C")
+		cancel()
+	}()
+
+	for i := 0; i < n; i++ {
+		go worker4(ctx, i, channel)
+	}
+
+	recordConstantly4(ctx, channel)
+
+}
