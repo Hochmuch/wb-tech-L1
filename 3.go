92056c026472d4ce2506fa32f059c163bdf4db73
hochmuch <egorrussian56@gmail.com>
2025-08-19 15:30:53 +0300

    Task 3 added

--- 3.go
+++ 3.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func recordConstantly(channel chan int) {
+	i := 0
+	for {
+		channel <- i
+		i++
+	}
+}
+
+func worker(id int, channel chan int) {
+	for {
+		x := <-channel
+		fmt.Printf("Worker #%v прочитал: %v\n", id, x)
+	}
+}
+
+func main() {
+	n, _ := strconv.Atoi(os.Args[1])
+	channel := make(chan int)
+
+	for i := 0; i < n; i++ {
+		go worker(i, channel)
+	}
+
+	recordConstantly(channel)
+
+}
