02d5287f09d0f7986a16684c1fffcb25a5f7dc2f
hochmuch <egorrussian56@gmail.com>
2025-08-27 22:46:48 +0300

    Task 25 added

--- 25.go
+++ 25.go
@@ -1,12 +1,29 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func sleep(duration time.Duration) {
 	timer := time.NewTimer(duration)
+	defer timer.Stop()
 	<-timer.C
 }
 
 func main() {
-	sleep(time.Second * 2)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		sleep(time.Second * 5)
+		fmt.Println("Прошло 5 секунд")
+	}()
+	for i := 1; i <= 4; i++ {
+		sleep(time.Second)
+		fmt.Printf("Прошло %v секунд\n", i)
+
+	}
+	wg.Wait()
 }
